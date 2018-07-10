eb3c9e91678e8cd959d68e352270907201e18c57
Nico <nox_blue@hotmail.com>
2018-07-10 12:16:24 +0200

    Ajouts/modifications des methodes search des controllers Agences, Clients et Destinations

--- Jen-Nico-BoVoyage/Jen-Nico-BoVoyage/Controllers/ClientsController.cs
+++ Jen-Nico-BoVoyage/Jen-Nico-BoVoyage/Controllers/ClientsController.cs
@@ -45,6 +45,40 @@ namespace Jen_Nico_BoVoyage.Controllers
         }
 
         /// <summary>
+        /// Récupère la liste des Clients en fonction de la civilité, du nom, du prénom, de l'adresse, du téléphone, de la date de naissance et/ou de l'email
+        /// </summary>
+        /// <param name="civilite"></param>
+        /// <param name="nom"></param>
+        /// <param name="prenom"></param>
+        /// <param name="adresse"></param>
+        /// <param name="telephone"></param>
+        /// <param name="dateNaissance"></param>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        [Route("api/Clients/search")]
+        public IQueryable<Client> GetSearch(string civilite = "", string nom = "", string prenom = "", string adresse = "", string telephone = "", DateTime? dateNaissance = null, string email="")
+        {
+            var liste = db.Clients.Where(x => !x.Deleted);
+            if (!string.IsNullOrWhiteSpace(civilite))
+                liste = liste.Where(x => x.Civilite.Contains(civilite));
+            if (!string.IsNullOrWhiteSpace(nom))
+                liste = liste.Where(x => x.Nom.Contains(nom));
+            if (!string.IsNullOrWhiteSpace(prenom))
+                liste = liste.Where(x => x.Prenom.Contains(prenom));
+            if (!string.IsNullOrWhiteSpace(adresse))
+                liste = liste.Where(x => x.Adresse.Contains(adresse));
+            if (!string.IsNullOrWhiteSpace(telephone))
+                liste = liste.Where(x => x.Telephone.Contains(telephone));
+            if (dateNaissance != null)
+                liste = liste.Where(x => x.DateNaissance == dateNaissance);
+            if (!string.IsNullOrWhiteSpace(email))
+                liste = liste.Where(x => x.Email.Contains(email));
+
+            return liste;
+        }
+
+
+        /// <summary>
         /// Modifie un Client grâce à un objet JSON
         /// </summary>
         /// <returns></returns>
