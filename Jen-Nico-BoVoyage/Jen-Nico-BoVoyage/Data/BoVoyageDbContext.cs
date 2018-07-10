3c258045b107ba618b56bfa6d5314269d87ede12
Nico <nox_blue@hotmail.com>
2018-07-10 09:50:16 +0200

    Création des DBSet

--- Jen-Nico-BoVoyage/Jen-Nico-BoVoyage/Data/BoVoyageDbContext.cs
+++ Jen-Nico-BoVoyage/Jen-Nico-BoVoyage/Data/BoVoyageDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Jen_Nico_BoVoyage.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -12,6 +13,11 @@ namespace Jen_Nico_BoVoyage.Data
         {
         }
 
-        //public DbSet<Agence> Agences { get; set; }
+        public DbSet<Agence> Agences { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Voyage> Voyages { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Dossier> Dossiers { get; set; }
     }
 }
