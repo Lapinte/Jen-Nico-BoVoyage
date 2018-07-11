faa96facee996cae2b42c1c8f26fc42f1fad82f6
Jennifer Martin <jennifer_martin@outlook.fr>
2018-07-11 17:46:11 +0200

    Crud Agences terminé et crud Dossiers en cours.

---
+++ Jen-Nico-BoVoyage/Jen-Nico-BoVoyage/Scripts/functionsAgence.js
@@ -0,0 +1,49 @@
+﻿function deleteAgence(id) {
+    $("#deleteModal").modal("show");
+    $("#confirm").on("click", function () {
+        $.ajax({
+            url: "http://localhost:54746/api/Agences/" + id,
+            method: 'delete',
+            success: function () {
+                window.location.href = "http://localhost:54746/Html/AfficherAgences.html";
+                alert("Agence supprimée");
+            }
+        });
+    });
+
+};
+
+function editAgence(id) {
+    let url = "http://localhost:54746/api/Agences/" + id;
+    $("#editModaForm").trigger("reset");
+    $("#editModal").modal();
+    $.ajax({
+        url: url,
+        method: 'GET',
+        success: function (data) {
+            $("#agenceID").val(data.ID);
+            $("#editNom").val(data.Nom);
+        }
+    });
+    $("#editModalForm").submit(function (ev) {
+        let obj = {
+            ID: $("#agenceID").val(),
+            Nom: $("#editNom").val()
+        };
+
+        $.ajax({
+            url: url,
+            method: 'PUT',
+            data: obj,
+            success: function () {
+                alert("Agence modifiée");
+                window.location.href = "http://localhost:54746/Html/AfficherAgences.html";
+                $("#editModal").modal("hide");
+            }
+
+        });
+        ev.preventDefault();
+    });
+
+
+};
