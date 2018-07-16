0056109044d83e85ee91f77ac651fcde7b0149f1
admin <admin@admin.com>
2018-07-16 17:29:23 +0200

    Tentative de récup du nom d'agence et destination dans voyage.html

---
+++ Jen-Nico-BoVoyage/Jen-Nico-BoVoyage/Scripts/functionsVoyage.js
@@ -0,0 +1,54 @@
+﻿function deleteDestination(id) {
+    $("#deleteModal").modal("show");
+    $("#confirm").on("click", function () {
+        $.ajax({
+            url: "http://localhost:54746/api/destinations/" + id,
+            method: 'delete',
+            success: function () {
+                window.location.href = "http://localhost:54746/Html/Destinations.html";
+                alert("Destination supprimée");
+            }
+        });
+    });
+};
+
+function editDestination(id) {
+    
+    let url = "http://localhost:54746/api/destinations/" + id;
+    $("#editModalForm").trigger("reset");
+    $("#editModal").modal();
+    $.ajax({
+        url: url,
+        method: 'GET',
+        success: function (data) {
+            $("#destinationID").val(data.ID);
+            $("#editContinent").val(data.Continent);
+            $("#editPays").val(data.Pays);
+            $("#editRegion").val(data.Region);
+            $("#editDescription").val(data.Description);
+        }
+    });
+    $("#editModalForm").submit(function (ev) {
+        let obj = {
+            ID: $("#destinationID").val(),
+            Continent: $("#editContinent").val(),
+            Pays: $("#editPays").val(),
+            Region: $("#editRegion").val(),
+            Description: $("#editDescription").val()
+        };
+
+        $.ajax({
+            url: url,
+            method: "PUT",
+            data: obj,
+            success: function () {
+                alert(`Destination modifiée`);
+                window.location.href = "http://localhost:54746/Html/Destinations.html";
+                $("#editModal").modal("hide");
+            }
+        });
+        ev.preventDefault();
+    });
+};
+
+
